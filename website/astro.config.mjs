cb3f5bfbb81b0778bcf9801f6a0229fa52bdb9ba
isaoosugi-lab <isao.osugi@gmail.com>
2026-06-28 22:49:50 +0900

    外部リンクを別タブで開くよう調整

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -1,7 +1,19 @@
 import { defineConfig } from "astro/config";
+import rehypeExternalLinks from 'rehype-external-links';
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://chrossworks.github.io",
   base: "/genshin-miliastra-guide",
+  markdown: {
+    rehypePlugins: [
+    [
+      rehypeExternalLinks,
+      {
+        target: '_blank',
+        rel: ['noopener', 'noreferrer'],
+      },
+    ],
+    ],
+  },
 });
